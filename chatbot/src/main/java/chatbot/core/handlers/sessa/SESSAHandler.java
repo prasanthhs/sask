6d8eb2d35b786903827059f144ba1a26ae333688
idreestahir <zmidrees@gmail.com>
2017-12-18 19:08:19 +0500

    Updating SESSAHandler Class

--- chatbot/src/main/java/chatbot/core/handlers/sessa/SESSAHandler.java
+++ chatbot/src/main/java/chatbot/core/handlers/sessa/SESSAHandler.java
@@ -1,5 +1,5 @@
 /**
- *
+ * 
  */
 package chatbot.core.handlers.sessa;
 import org.apache.log4j.Logger;
@@ -16,68 +16,49 @@ import java.util.*;
  *
  */
 public class SessaHandler extends Handler {
-        // Handle Hawk Service.
-        private static Logger log = Logger.getLogger(SessaHandler.class.getName());
-        private static final String URL = "http://localhost:7070/search?query="; // URL
+	// Handle Hawk Service.
+	private static Logger log = Logger.getLogger(SessaHandler.class.getName());
+	private static final String URL = "http://localhost:7070/search?query="; // URL
 
-        public SessaHandler() {
+	public SessaHandler() {
 
-        }
+	}
 
-        private String generateResponse(String incomingResponse) throws JsonProcessingException, IOException {
-                incomingResponse = incomingResponse.substring(1,incomingResponse.length()-1);
-                log.info(incomingResponse);
-                String[] parts = incomingResponse.split(",");
-                String response = "[";
-                for (int i = 0; i < parts.length; ++i)
-                {
-                        response += "{\"URI\":" + parts[i] + "},";
-                }
-                response = response.substring(0, response.length() - 1);
+	private String generateResponse(String incomingResponse) throws JsonProcessingException, IOException {
+		incomingResponse = incomingResponse.substring(1,incomingResponse.length()-1);
+		log.info(incomingResponse);
+		String[] parts = incomingResponse.split(",");
+		String response = "[";
+		for (int i = 0; i < parts.length; ++i)
+		{
+			response += "{\"URI\":" + parts[i] + "},";
+		}
+		response = response.substring(0, response.length() - 1);
                 response += "]";
-                return response;
-        private static final String URL = "http://localhost:7070/search?query="; // URL
+		return response;
+	}
+	public String search(IncomingRequest request) throws JsonProcessingException, IOException {
+		try {
+			String query = request.getRequestContent()
+			                      .get(0)
+			                      .getText();
+			String sendText = URL + query;
+			String response = sendHTTPRequest(sendText);
+			String output = generateResponse(response);
+			return output;
+		} catch (JsonProcessingException e) {
+			// Check if we can create a logger.
+			log.error("search, JsonProcessingException in handling QA Queries,Stack Trace=" + e.getMessage());
+			return "error";
+		} catch (IOException e) {
+			// Check if we can create a logger.
+			log.error("search, IOException in handling QA Queries,Stack Trace=" + e.getMessage());
+			return "error";
+		} catch (Exception e) {
+			// Check if we can create a logger.
+			log.error("search, Exception in handling QA Queries,Stack Trace=" + e.getMessage());
+			return "error";
 
-        public SessaHandler() {
-
-        }
-
-        private String generateResponse(String incomingResponse) throws JsonProcessingException, IOException {
-                incomingResponse = incomingResponse.substring(1,incomingResponse.length()-1);
-                log.info(incomingResponse);
-                String[] parts = incomingResponse.split(",");
-                String response = "[";
-                for (int i = 0; i < parts.length; ++i)
-                {
-                        response += "{\"URI\":" + parts[i] + "},";
-                }
-                response = response.substring(0, response.length() - 1);
-                response += "]";
-                return response;
-        }
-        public String search(IncomingRequest request) throws JsonProcessingException, IOException {
-                try {
-                        String query = request.getRequestContent()
-                                              .get(0)
-                                              .getText();
-                        String sendText = URL + query;
-                        String response = sendHTTPRequest(sendText);
-                        String output = generateResponse(response);
-                        return output;
-                } catch (JsonProcessingException e) {
-                        // Check if we can create a logger.
-                        log.error("search, JsonProcessingException in handling QA Queries,Stack Trace=" + e.getMessage());
-                        return "error";
-                } catch (IOException e) {
-                        // Check if we can create a logger.
-                        log.error("search, IOException in handling QA Queries,Stack Trace=" + e.getMessage());
-                        return "error";
-                } catch (Exception e) {
-                        // Check if we can create a logger.
-                        log.error("search, Exception in handling QA Queries,Stack Trace=" + e.getMessage());
-                        return "error";
-
-                }
-        }
+		}
+	}
 }
-
